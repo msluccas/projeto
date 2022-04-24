c9094b041fbdf43e6eab145913a195b6968f2284
msluccas <lucagomes90@gmail.com>
2022-04-23 22:43:42 -0300

    páginas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 function Home() {
-    return <div><h1>Lucas Araujo</h1></div>
+    return <div><h1>Lucas Araujo</h1>
+    <h2>PROGRAMAÇÃO TESTE</h2>
+    <p>ESTA PÁGINA É PARA TESTE DE PROGRAMAÇÃO</p>
+    </div>
 }
 export default Home
